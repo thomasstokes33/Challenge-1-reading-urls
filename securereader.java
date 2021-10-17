d8dc2c178341df22484f5b47f0bf06db4a5cd151
thomasstokes33 <60468552+thomasstokes33@users.noreply.github.com>
2021-10-17 18:42:36 +0100

    Create securereader.java

---
+++ securereader.java
@@ -0,0 +1,46 @@
+//radix is the base
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.net.*;
+import java.util.regex.*;
+public class GetName {
+    public static void main(String[] args) {
+        System.out.print("Type an input ");
+        BufferedReader brInp=new BufferedReader(new InputStreamReader(System.in));
+        String username;
+        try{
+            username=brInp.readLine();
+        } catch (IOException ioe) {
+            username="";
+        }
+        String newurl= "https://www.secure.ecs.soton.ac.uk/people/";
+        newurl=newurl+username;
+        
+        System.out.println(newurl);
+        try {
+            URL fetcher= new URL(newurl);
+            System.out.println(fetcher.getContent());
+            URLConnection connection=fetcher.openConnection();
+            BufferedReader contents=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String html;
+            String finalStr="";
+            while ((html=contents.readLine())!=null) {
+                
+                finalStr+=html;
+            }
+           
+
+
+        } catch (MalformedURLException mue) {
+            ;
+        } catch (IOException ioe){
+            ;
+        }
+        
+        
+        
+
+
+    }
+}
